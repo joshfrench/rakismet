8d60b07b979a6b100a64e7a4810f41824a979a82
Josh French <josh@vitamin-j.com>
2009-03-21 13:06:03 -0400

    Version bump

--- lib/rakismet.rb
+++ lib/rakismet.rb
@@ -44,7 +44,7 @@ module Rakismet
   NoBinding = Class.new(NameError)
   
   HEADERS = {
-    'User-Agent' => "Rails/#{Rails::VERSION::STRING} | Rakismet/0.2.1",
+    'User-Agent' => "Rails/#{Rails::VERSION::STRING} | Rakismet/0.2.2",
     'Content-Type' => 'application/x-www-form-urlencoded'
   }
 end
