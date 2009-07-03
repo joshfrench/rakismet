71eeb6cb718225a0debeee5c5eecfe7eeaf3fc95
Josh French <josh@vitamin-j.com>
2009-07-03 11:48:22 -0400

    Default host and docs

--- install.rb
+++ install.rb
@@ -1,3 +1,5 @@
 File.open(File.join(RAILS_ROOT, %w(config initializers rakismet.rb)) , 'w') do |f|
-  f.puts "Rakismet::KEY = ''\nRakismet::URL = ''"
+  f.puts "Rakismet::KEY  = ''"
+  f.puts "Rakismet::URL  = ''"
+  f.puts "Rakismet::HOST = 'rest.akismet.com'"
 end
