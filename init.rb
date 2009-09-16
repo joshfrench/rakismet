7bf40e50acc6e42159dcfc24d5e1a87f01255469
michael <michael@airbook.local>
2009-09-16 12:28:23 +1000

    Updated init to add rakismet to the load_once path for compatability with rails engines which need reloadable plugins set to true

--- init.rb
+++ init.rb
@@ -1,2 +1,10 @@
+# When using reloadable plugins (for engines), rakismet breaks. 
+# This adds rakismet to the load_once path in development mode so that it doesn't break.
+
+if RAILS_ENV == "development"
+  RAILS_DEFAULT_LOGGER.info("[Rakismet] Adding rakismet to load once path")
+  ActiveSupport::Dependencies.load_once_paths << File.join(RAILS_ROOT, "vendor/plugins/rakismet/lib")
+end
+
 ActionController::Base.send :include, Rakismet::ControllerExtensions
 ActiveRecord::Base.send :include, Rakismet::ModelExtensions
