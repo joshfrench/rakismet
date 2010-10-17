443a7948bb01d0c15c3822edf5ef4d3374e4b5c5
Josh French <josh@digitalpulp.com>
2010-10-17 13:47:43 -0400

    Make middleware optional

--- lib/rakismet/railtie.rb
+++ lib/rakismet/railtie.rb
@@ -3,15 +3,17 @@ require 'rakismet'
 
 module Rakismet
   class Railtie < Rails::Railtie
+
       config.rakismet = ActiveSupport::OrderedOptions.new
       config.rakismet.host = 'rest.akismet.com'
-      initializer 'rakismet.get_config', :after => :load_config_initializers do |app|
+      config.rakismet.use_middleware = true
+
+      initializer 'rakismet.setup', :after => :load_config_initializers do |app|
         Rakismet.key = app.config.rakismet.key
         Rakismet.url = app.config.rakismet.url
         Rakismet.host = app.config.rakismet.host
+        app.middleware.use Rakismet::Middleware if app.config.rakismet.use_middleware
       end
-      initializer 'rakismet.add_middleware' do |app|
-        app.middleware.use Rakismet::Middleware
-      end
+
   end
 end
