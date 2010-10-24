9902eed847f9a2b058677c3d16ed3d6f99da2ff1
Alex Crichton <alex@alexcrichton.com>
2010-10-24 18:06:43 -0400

    We can't access the 'key' attribute in the hash with the method call to 'key' because it's already a method defined on the hash.
    This causes the method call to require an argument and raise an error. Instead, just access it like any normal hash and we can get the hash value for 'key' instead of calling that method.

--- lib/rakismet/railtie.rb
+++ lib/rakismet/railtie.rb
@@ -9,9 +9,9 @@ module Rakismet
       config.rakismet.use_middleware = true
 
       initializer 'rakismet.setup', :after => :load_config_initializers do |app|
-        Rakismet.key = app.config.rakismet.key
-        Rakismet.url = app.config.rakismet.url
-        Rakismet.host = app.config.rakismet.host
+        Rakismet.key = app.config.rakismet[:key]
+        Rakismet.url = app.config.rakismet[:url]
+        Rakismet.host = app.config.rakismet[:host]
         app.middleware.use Rakismet::Middleware if app.config.rakismet.use_middleware
       end
 
